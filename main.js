63d34a6998d637972e56848ae040bcc017e261ab
victorakaps <49690593+victorakaps@users.noreply.github.com>
2020-08-04 23:09:41 +0530

    backend-completed

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ const results_container = document.querySelector("#results")
 
 async function getResults(query){
     let response = await fetch("http://localhost:3000/search?s=" + query);
-    let data = await response.text();
+    let data = await response.json();
     return data;
 }
 
 function handleOnSubmit(event) {
-    event.preventDefault(); // stop page changing to #, which will reload the page
+    event.preventDefault();
 
     const query = form.querySelector("input").value.trim()
 
@@ -26,11 +26,10 @@ async function doSearch(query) {
     results_container.innerHTML = `<span class="loader">Searching</span>`;
 
     const json = await getResults(query); 
-    console.log(json);
     let results = [];
 
     for(let track of json) {
-        results.push(`<a href="./${track.Main_URL}" class="track"><img src="${track.Pic_URL}"> <span>${track.Title}</span></a>`);
+        results.push(`<a href="${track.url}" class="track" download="${track.title}.mp3 target="_blank"><img src=${track.pic}> <span>${track.title}</span></a>`);
     }
 
     results_container.innerHTML = results.join(' ');
